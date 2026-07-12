405baed1bfa5fbbf19921f449c7edf61c2a3b06b
Uday Dey <udayday4.ud@gmail.com>
2026-07-13 01:18:42 +0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import Rail from "@/components/Rail";
 import ThemeToggle from "@/components/ThemeToggle";
 import Start from "@/components/Start";
 import Systems from "@/components/Systems";
+import PullQuote from "@/components/PullQuote";
 import Connects from "@/components/Connects";
 import Record from "@/components/Record";
 import Contact from "@/components/Contact";
@@ -23,6 +24,7 @@ export default function Home() {
       <main className="pt-14 lg:pt-0">
         <Start />
         <Systems />
+        <PullQuote />
         <Connects />
         <Record />
         <Contact />
